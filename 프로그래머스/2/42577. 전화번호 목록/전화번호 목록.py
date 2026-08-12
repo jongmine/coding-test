876d524f2d0ffc16a54fb16bd9fadcdb2212f21a
Jongmin Choi <jongmine.ch@gmail.com>
2026-08-12 14:50:41 +0900

    [level 2] Title: 전화번호 목록, Time: 385.34 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,12 @@
 def solution(phone_book):
-    phone_set = set(phone_book)
-    
+    phone_hash = dict()
+        
     for p in phone_book:
+        phone_hash[p] = True
+    for p in phone_book:        
         for i in range(1, len(p)):
             prefix = p[:i]
-            if prefix in phone_set:
+            if prefix in phone_hash:
                 return False
             
     return True
