be97995cf652585a0a635792b8e7ff35886da2e3
Jongmin Choi <jongmine.ch@gmail.com>
2026-09-01 16:25:42 +0900

    Delete Python3/Easy/0349-intersection-of-two-arrays directory

--- Python3/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set1 = set(nums1)
-        set2 = set(nums2)
-
-        intersection_set = set1.intersection(set2)
-
-        return list(intersection_set)
