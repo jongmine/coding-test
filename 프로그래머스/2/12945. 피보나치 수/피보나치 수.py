6e5e3f0eb0b03a08c4ea29fe543c1c4e5f6d3f9f
Jongmin Choi <jongmine.ch@gmail.com>
2026-09-14 01:06:09 +0900

    Change Fibonacci calculation to iterative method
    Refactor Fibonacci function to use iterative approach instead of recursion and update memoization.

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,27 +1,30 @@
-import sys
-sys.setrecursionlimit(200000)
+# 재귀 호출 깊이 제한 증가
+# import sys
+# sys.setrecursionlimit(10 ** 6)
 
 
 def solution(n):
-    
+    # memorization for DP
     memo = {
+        0: 0,
         1: 1,
-        2: 1,
-        3: 2,
-        4: 3,
-        5: 5
+        2: 1
     }
     
-    def fibonacci(n, memo):
+# 1. 재귀    
+#     def fibonacci(n, memo):
+#         if n in memo:
+#             return memo[n] % 1234567
         
-        # memo에 해당 값이 있다면 계산 하지 않고 반환 -> 재귀 오류 방지
-        if n in memo:
-            return memo[n]
+#         # 점화식: F(n) = F(n-2) + F(n-1)
+#         answer = fibonacci(n - 2, memo) + fibonacci(n - 1, memo)
+#         memo[n] = answer
         
-        # 점화식: F(n) = F(n-2) + F(n-1)
-        answer = fibonacci(n - 2, memo) + fibonacci(n - 1, memo)
-        memo[n] = answer
-        
-        return answer % 1234567
+#         return answer % 1234567
     
-    return fibonacci(n, memo)
+#     return fibonacci(n, memo)
+    
+    # 2. 반복문        
+    for i in range(3, n + 1):
+        memo[i] = memo[i - 2] + memo[i - 1]    
+    return memo[i] % 1234567
